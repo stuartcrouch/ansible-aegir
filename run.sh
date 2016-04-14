c435b193fb300a64506bb1d420d729e80a3b2322
root <root@aegirajm.vlu0gjbdl5berfcacelr55w0fc.ax.internal.cloudapp.net>
2016-04-14 14:15:17 +0000

    Add setenforce 0 and a start of apache at the end of the install

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+setenforce 0
 
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install ansible
 
 ansible-playbook -i ./inventory aegir.yml
+
+systemctl start httpd
